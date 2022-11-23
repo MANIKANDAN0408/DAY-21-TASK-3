4149ef5d63d1365ef89ec44de0bbc97614463834
MANIKANDAN0408 <115692293+MANIKANDAN0408@users.noreply.github.com>
2022-11-23 17:46:53 +0530

    Add files via upload

---
+++ DAY 21 TASK 3/script.js
@@ -0,0 +1,36 @@
+//FUNCTION TO GET THE DATA
+
+async function foo(){
+    let url=await fetch("https://api.thedogapi.com/v1/breeds")
+    let res=await url.json()
+    res.forEach((e) => {
+      try {
+  
+        
+//CREATE ELEMENT TO DISPLAY THE DATA  
+
+        let div=document.createElement("div")
+        div.className="box"
+        div.innerHTML=`<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${e.name}</h5>
+        </div>
+        <img src="${e.image.url}" class="card-img-top" alt="image loading..">
+        <div><span><b>Breed-Group  :</b> </span> ${e.breed_group}</div><br>
+        <div><span><b>Life-Span  :</b> </span>  ${e.life_span}</div><br>
+        <div><span><b>Bred-For : </b></span> ${e.bred_for}</div><br>
+      </div>`
+  
+      
+//TO APPEND THE DATA
+
+      document.body.append(div)
+      } catch (error) {
+        alert(error)
+      }
+    });
+  }
+  
+ //TO CALL THE FUCTION
+  
+  foo()
